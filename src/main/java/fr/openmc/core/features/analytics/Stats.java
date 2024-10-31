427fa1f9b51f7468458bd18c10df109831b51255
MathiasDPX <mathias@dupeux.net>
2024-10-31 22:32:16 +0100

    Add get method to Stats

--- src/main/java/fr/openmc/core/features/analytics/Stats.java
+++ src/main/java/fr/openmc/core/features/analytics/Stats.java
@@ -5,12 +5,43 @@ import fr.openmc.core.utils.database.DatabaseManager;
 import org.bukkit.Bukkit;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
 public enum Stats {
     SESSION,
     ;
+
+    /**
+     * Return the stats for a player
+     * @param uuid Player
+     * @param defaultValue The value that will get returned if analytics is disabled or didn't work
+     * @return The stats of the player, if unavailable, it will return defaultValue
+     */
+    public int get(UUID uuid, int defaultValue) {
+        if (!AnalyticsManager.isEnabled()) return defaultValue;
+        try {
+            PreparedStatement statement = DatabaseManager.getConnection().prepareStatement("SELECT value FROM stats WHERE player = ? AND scope = ? LIMIT 1");
+            statement.setString(1, uuid.toString());
+            statement.setString(2, this.name());
+            ResultSet resultSet = statement.executeQuery();
+
+            return resultSet.getInt(0);
+        } catch (SQLException err) {
+            err.printStackTrace();
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Return the stats for a player
+     * @param uuid Player
+     * @return The stats of the player, if unavailable, it will return `0`
+     */
+    public int get(UUID uuid) {
+        return get(uuid, 0);
+    }
 
     /**
      * Increment a stats for a player
