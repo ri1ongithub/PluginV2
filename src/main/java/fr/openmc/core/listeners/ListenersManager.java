1ff8a18e141911ab03a65777838935a90c7e9d99
iambibi <lethiebaut2@gmail.com>
2024-11-02 13:37:30 +0100

    Make Review Changes

--- src/main/java/fr/openmc/core/listeners/ListenersManager.java
+++ src/main/java/fr/openmc/core/listeners/ListenersManager.java
@@ -10,7 +10,8 @@ public class ListenersManager {
     public ListenersManager() {
         registerEvents(
                 new SessionsListener(),
-                new PlayerListener()
+                new JoinMessageListener(),
+                new QuitMessageListener()
         );
     }
 
--- src/main/java/fr/openmc/core/listeners/QuitMessageListener.java
+++ src/main/java/fr/openmc/core/listeners/QuitMessageListener.java
@@ -2,27 +2,14 @@ package fr.openmc.core.listeners;
 
 import fr.openmc.core.OMCPlugin;
 import net.kyori.adventure.text.Component;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.query.QueryOptions;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import net.luckperms.api.model.user.User;
-import net.luckperms.api.query.QueryOptions;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-public class PlayerListener implements Listener {
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-
-        User userlp = OMCPlugin.getInstance().lpApi.getUserManager().getUser(player.getUniqueId());
-        QueryOptions queryOptions = OMCPlugin.getInstance().lpApi.getContextManager().getQueryOptions(userlp).orElse(QueryOptions.defaultContextualOptions());
-
-
-        event.joinMessage(Component.text("§8[§a§l+§8] §r" + (userlp.getCachedData().getMetaData(queryOptions).getPrefix() != null ? userlp.getCachedData().getMetaData(queryOptions).getPrefix().replace("&", "§") : "") + "" + player.getName()));
-    }
-
+public class QuitMessageListener implements Listener  {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
