794df2ccfa6c1a024bff9d8a02eb539a586c69f3
MathiasDPX <mathias@dupeux.net>
2024-12-05 10:44:54 +0100

    Add custom items

--- src/main/java/fr/openmc/core/utils/customitems/CustomItem.java
+++ src/main/java/fr/openmc/core/utils/customitems/CustomItem.java
@@ -1,10 +1,20 @@
 package fr.openmc.core.utils.customitems;
 
+import lombok.Getter;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 public abstract class CustomItem {
     public abstract ItemStack getVanilla();
     public abstract ItemStack getItemsAdder();
+    @Getter private final String name;
+
+    public CustomItem(String name) {
+        this.name = name;
+        CustomItemRegistry.register(name, this);
+    }
 
     /**
      * Order:
@@ -13,7 +23,18 @@ public abstract class CustomItem {
      * @return Best ItemStack to use for the server
      */
     public ItemStack getBest() {
-        ItemStack itemsAdder = getItemsAdder();
-        return itemsAdder != null ? itemsAdder : getVanilla();
+        ItemStack item = null;
+        if (CustomItemRegistry.hasItemsAdder()) item = getItemsAdder();
+
+        if (item == null) {
+            item = getVanilla();
+        }
+
+        ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+        pdc.set(CustomItemRegistry.customNameKey, PersistentDataType.STRING, name);
+        item.setItemMeta(meta);
+
+        return item;
     }
 }
