71fc71743c7dc319021910c1d84093b34f10f660
Gabriel <81768313+gab4000@users.noreply.github.com>
2025-04-20 11:12:51 +0200

    Modification du freeze pour verifier les deconnexions et réparation des messages bugués (#252)

--- src/main/java/fr/openmc/core/utils/freeze/FreezeListener.java
+++ src/main/java/fr/openmc/core/utils/freeze/FreezeListener.java
@@ -1,5 +1,9 @@
 package fr.openmc.core.utils.freeze;
 
+import fr.openmc.core.utils.messages.MessageType;
+import fr.openmc.core.utils.messages.MessagesManager;
+import fr.openmc.core.utils.messages.Prefix;
+import net.kyori.adventure.text.Component;
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
@@ -7,6 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
@@ -25,9 +30,23 @@ public class FreezeListener implements Listener {
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
 		if (FreezeManager.FROZEN_PLAYERS.contains(player)) {
+			if (event.getReason() != PlayerQuitEvent.QuitReason.DISCONNECTED) {
+				FreezeManager.contactFreezer(event.getReason());
+				return;
+			}
 			Date banDuration = new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(30));
 			Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), "Déconnexion en étant freeze !", banDuration, "Anti Déco Freeze");
 			FreezeManager.FROZEN_PLAYERS.remove(player);
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		if (FreezeManager.FROZEN_PLAYERS.contains(player)) {
+			player.setInvulnerable(true);
+			player.sendTitle("§4Vous êtes freeze", "§5Si vous vous déconnectez, vous serez banni");
+			MessagesManager.sendMessage(player, Component.text("§4Vous avez été freeze"), Prefix.OPENMC, MessageType.WARNING, true);
 		}
 	}
 
--- src/main/java/fr/openmc/core/utils/freeze/FreezeManager.java
+++ src/main/java/fr/openmc/core/utils/freeze/FreezeManager.java
@@ -6,7 +6,9 @@ import fr.openmc.core.utils.messages.MessagesManager;
 import fr.openmc.core.utils.messages.Prefix;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 public class FreezeManager {
 	
 	public static final List<Player> FROZEN_PLAYERS = new ArrayList<>();
+	private static Player player;
 	
 	public FreezeManager() {
 		Bukkit.getServer().getPluginManager().registerEvents(new FreezeListener(), OMCPlugin.getInstance());
@@ -26,22 +29,40 @@ public class FreezeManager {
 	 * @param target The player to freeze/unfreeze
 	 */
 	public static void switchFreeze(Player player, Player target) {
+		FreezeManager.player = player;
 		if (target == null) {
 			MessagesManager.sendMessage(player, Component.text("§4Joueur introuvable"), Prefix.OPENMC, MessageType.ERROR, false);
 		} else {
-			String freezed;
-
 			if (FROZEN_PLAYERS.contains(target)) {
+				target.setInvulnerable(false);
 				FROZEN_PLAYERS.remove(target);
-				freezed = "freeze";
+				MessagesManager.sendMessage(player, Component.text("§2Vous avez unfreeze §6 " + target.getName()), Prefix.OPENMC, MessageType.SUCCESS, false);
+				MessagesManager.sendMessage(target, Component.text("§4Vous avez été unfreeze"), Prefix.OPENMC, MessageType.INFO, true);
 			} else {
+				target.setInvulnerable(true);
+				Location location = target.getLocation();
+				location.setY(location.getWorld().getHighestBlockYAt(location) + 1);
+				target.teleport(location);
 				FROZEN_PLAYERS.add(target);
 				target.sendTitle("§4Vous êtes freeze", "§5Si vous vous déconnectez, vous serez banni");
-				freezed = "unfreeze";
+				MessagesManager.sendMessage(player, Component.text("§2Vous avez freeze §6" + target.getName()), Prefix.OPENMC, MessageType.SUCCESS, false);
+				MessagesManager.sendMessage(target, Component.text("§4Vous avez été freeze"), Prefix.OPENMC, MessageType.WARNING, true);
 			}
-
-			MessagesManager.sendMessage(player, Component.text("§2Vous avez " + freezed + "§6" + target.getName()), Prefix.OPENMC, MessageType.SUCCESS, false);
-			MessagesManager.sendMessage(target, Component.text("§4Vous avez été " + freezed), Prefix.OPENMC, MessageType.INFO, true);
+		}
+	}
+	
+	/**
+	 * Contact the freezer to explain the reason of the disconnection
+	 *
+	 * @param reason The reason of the disconnection
+	 */
+	public static void contactFreezer(PlayerQuitEvent.QuitReason reason) {
+		if (player == null) return;
+		switch (reason) {
+			case KICKED -> MessagesManager.sendMessage(player, Component.text("§4Le joueur a été kick"), Prefix.OPENMC, MessageType.INFO, true);
+			case TIMED_OUT -> MessagesManager.sendMessage(player, Component.text("§4Le joueur a été time out"), Prefix.OPENMC, MessageType.INFO, true);
+			case ERRONEOUS_STATE -> MessagesManager.sendMessage(player, Component.text("§4Le joueur a eu une erreur de connexion"), Prefix.OPENMC, MessageType.INFO, true);
+			default -> MessagesManager.sendMessage(player, Component.text("§4Le joueur a été déconnecté"), Prefix.OPENMC, MessageType.INFO, true);
 		}
 	}
 }
